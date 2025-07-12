d2ce76951279fd93d6e7fb53e50ed3759f62e825
Grey Rhino Security <95465104+GreyRhinoSecurity@users.noreply.github.com>
2025-07-11 18:00:08 -0700

    Update install_catnip.sh

--- install_catnip.sh
+++ install_catnip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# === CatNip v3.3 Installer ===
+# === CatNip v3.2.1 Installer ===
 REPO_URL="https://github.com/GreyRhinoSecurity/CatNip"
 DEB_FILE="catnip_3.2.1_all.deb"
 
@@ -23,5 +23,5 @@ echo "🧹 Cleaning up..."
 cd ~
 rm -rf "$TEMP_DIR"
 
-echo "✅ CatNip v3.3 installed! Run it using:"
+echo "✅ CatNip v3.2.1 installed! Run it using:"
 echo "    catnip"
